4d2e0425637725c280069b8dd79459fee6cdc269
softwaretestlab <softwaretestlab@hotmail.com>
2022-12-02 17:05:51 +1100

    JIRA-3456

--- src/test/java/LoginPage/Login.java
+++ src/test/java/LoginPage/Login.java
@@ -7,5 +7,6 @@ public class Login {
     @Test
     public void NavToLogin(){
         System.out.println("navigate to login screen");
+        System.out.println("navigate to new login screen");
     }
 }
